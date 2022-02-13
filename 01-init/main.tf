7e96441be9622e2a1560a2effb46b53ec152ba3d
Jurijs Kolomijecs <jurikolo@yandex.com>
2022-02-13 17:40:35 +0200

    01, init

---
+++ 01-init/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "jurikolo"
+
+    workspaces {
+      name = "aws-test-1"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_key_id
+}
+
+data "aws_s3_bucket" "jurikolo_name" {
+  bucket = "jurikolo.name"
+}
+
+output "s3_arn" {
+  value = data.aws_s3_bucket.jurikolo_name.arn
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # terraform-aws
 Experiments with AWS using Terraform
+
+## Terraform Cloud
+This project uses [Terraform Cloud](https://www.terraform.io/cloud-docs).
+Due to this fact files like `terraform.tfvars` doesn't work by default, you shall configure variables in the cloud.
