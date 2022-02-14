5f7dc2dc8821b5e97304ec0729e3db67f82b0363
Jurijs Kolomijecs <jurikolo@yandex.com>
2022-02-14 21:22:25 +0200

    02, resource provisioning

---
+++ 02-resource-provisioning/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  cloud {
+    organization = "jurikolo"
+
+    workspaces {
+      name = "aws-test-1"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "eu-central-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_key_id
+}
+
+resource "aws_dynamodb_table" "dynamodb" {
+  name           = "Terraform"
+  billing_mode   = "PROVISIONED"
+  write_capacity = 1
+  read_capacity  = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  ttl {
+    enabled        = true
+    attribute_name = "ttl"
+  }
+
+  tags = {
+    Name       = "Terraform"
+    CostCenter = "Terraform"
+  }
+}
+
+output "dynamodb_arn" {
+  value = aws_dynamodb_table.dynamodb.arn
+}
--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Experiments with AWS using Terraform
 ## Terraform Cloud
 This project uses [Terraform Cloud](https://www.terraform.io/cloud-docs).
 Due to this fact files like `terraform.tfvars` doesn't work by default, you shall configure variables in the cloud.
+
+## AWS resources
+Resources provisioned are part of always free "Free Tier". More details: [link](https://aws.amazon.com/free/?all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=tier%23always-free&awsf.Free%20Tier%20Categories=*all) 
